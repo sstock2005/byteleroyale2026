e66789db6ce453869c9ace30fe11dc74faf406a0
sstock2005 <144393153+sstock2005@users.noreply.github.com>
2026-02-07 13:43:58 -0600

    success! first coin

--- base_client.py
+++ base_client.py
@@ -10,13 +10,41 @@ from game.utils.vector import Vector
 
 from math import dist
 
-# def get_closest_goal(u: Vector, l):
-#     maximum = float(1000)
-#     for v in l:
-#         d = dist(u.as_tuple(), v.as_tuple())
-#          if d <= maximum:
-#             maximum = d
-            
+def get_closest_goal(u: Vector, l):
+    """Helper method to determine the closest goal given a vector and a list of goal vectors"""
+    maximum = float(1000)
+    goal = None
+    for v in l:
+        d = dist(u.as_tuple(), v.as_tuple())
+        if d <= maximum:
+            maximum = d
+            goal = v
+    return goal
+
+def determine_movement(current_position: Vector, goal_position: Vector, world: GameBoard, my_avatar: Avatar) -> ActionType | None:
+    """Helper method to determine the next movement action based on current position and goal position."""
+    if current_position.y > goal_position.y:
+        # need to move up
+        new_position = Vector(current_position.x, current_position.y - 1)
+        if world.can_object_occupy(new_position, my_avatar):
+            return ActionType.MOVE_UP
+    if current_position.y < goal_position.y:
+        # need to move down
+        new_position = Vector(current_position.x, current_position.y + 1)
+        if world.can_object_occupy(new_position, my_avatar):
+            return ActionType.MOVE_DOWN
+    if current_position.x < goal_position.x:
+        # need to move right
+        new_position = Vector(current_position.x + 1, current_position.y)
+        if world.can_object_occupy(new_position, my_avatar):
+            return ActionType.MOVE_RIGHT
+    if current_position.x > goal_position.x:
+        # need to move left
+        new_position = Vector(current_position.x - 1, current_position.y)
+        if world.can_object_occupy(new_position, my_avatar):
+            return ActionType.MOVE_LEFT
+    return None
+
 class Client(UserClient):
 
     def __init__(self):
@@ -37,21 +65,11 @@ class Client(UserClient):
         :param world:       Generic world information
         """
         
-        match turn:
-            case 1:
-                return [ActionType.MOVE_RIGHT]
-            case 2:
-                return [ActionType.MOVE_RIGHT]
-            case 3:
-                return [ActionType.MOVE_RIGHT]
-            case 4:
-                return [ActionType.MOVE_DOWN]
-            case 5:
-                return [ActionType.MOVE_DOWN]
-            case 6:
-                return [ActionType.MOVE_DOWN]
-            case 7:
-                return [ActionType.MOVE_DOWN]
-            case 8:
-                return [ActionType.MOVE_DOWN]
+        current_position = avatar.position
+        coins = world.get_objects(ObjectType.COIN_SPAWNER).keys()
+        closest_coin = get_closest_goal(current_position, coins)
+        movement_action = determine_movement(current_position, closest_coin, world, avatar)
+        
+        if movement_action is not None:
+            return [movement_action]
         return []
